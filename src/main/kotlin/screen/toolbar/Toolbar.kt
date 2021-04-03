71796600849bf4d0bf1b94a9ebf115eb5db8cb17
YuriZhuravlev <zhuravlevyuri2000@gmail.com>
2021-04-03 21:17:11 +0300

    Добавлен тулбар c необходимой функциональностью

--- src/main/kotlin/screen/toolbar/Toolbar.kt
+++ src/main/kotlin/screen/toolbar/Toolbar.kt
@@ -1,8 +1,21 @@
 package screen.toolbar
 
-import androidx.compose.runtime.mutableStateOf
 import screen.CodeViewer
 
-class Toolbar(codeViewer: CodeViewer) {
+class Toolbar(val codeViewer: CodeViewer) {
+    private val success = "Ах да, я ж нихуя не сделал анализатор"
+    private val failed = "Я нихуя не сделал, но походу ты пытаешься наебать меня (файл не .pas)"
     val active = codeViewer.editors.active
+    var result = ""
+
+    fun checkText(onSuccess: () -> Unit, onFailure: () -> Unit) {
+        //TODO
+        if (codeViewer.editors.active?.fileName?.endsWith(".pas") == true) {
+            result = success
+            onSuccess()
+        } else {
+            result = failed
+            onSuccess()
+        }
+    }
 }
--- src/main/kotlin/screen/toolbar/ToolbarView.kt
+++ src/main/kotlin/screen/toolbar/ToolbarView.kt
@@ -8,12 +8,14 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun ToolbarView(model: Toolbar) {
     var textButton by remember { mutableStateOf("Проверить") }
+    var isChecked by remember { mutableStateOf(false)}
     Box(
         modifier = Modifier
             .height(36.dp)
@@ -30,9 +32,22 @@ fun ToolbarView(model: Toolbar) {
                         .padding(horizontal = 10.dp)
                         .clickable(true) {
                             textButton = "Проверка"
+                            model.checkText({isChecked = true; textButton = "Проверить"}, {})
                         },
                     color = Color.White,
                     fontSize = 12.sp
+                )
+            }
+
+            if (isChecked) {
+                Text(
+                    text = model.result,
+                    modifier = Modifier
+                        .align(Alignment.CenterVertically)
+                        .padding(horizontal = 10.dp),
+                    color = Color.LightGray,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.W700
                 )
             }
         }
