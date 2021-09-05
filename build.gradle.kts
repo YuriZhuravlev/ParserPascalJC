f619915e7cd8880c6051f31ad97653a740becf68
YuriZhuravlev <zhuravlevyuri2000@gmail.com>
2021-09-05 17:18:01 +0300

    Обновление версий компоуза и котлина

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,22 +3,22 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.31"
-    id("org.jetbrains.compose") version "0.3.2"
+    kotlin("jvm") version "1.5.21"
+    id("org.jetbrains.compose") version "1.0.0-alpha3"
 }
 
 group = "com.zhuravlev"
 version = "1.0.0"
 
 repositories {
-    jcenter()
+    google()
     mavenCentral()
     maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
 }
 
 dependencies {
     implementation(compose.desktop.currentOs)
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
 }
 
 tasks.withType<KotlinCompile>() {
